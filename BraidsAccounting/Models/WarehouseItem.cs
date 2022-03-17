20cab96e5046b5e425e904483e2484f06484739e
Виталий Никитин <nikitin-vitalii@rambler.ru>
2022-03-17 20:01:32 +1000

    Начало

---
+++ BraidsAccounting/Models/WarehouseItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BraidsAccounting.Models
+{
+    public class WarehouseItem
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public Item Item { get; set; } = null!;
+        public int Count { get; set; }
+    }
+}
---
+++ BraidsAccounting/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using BraidsAccounting.Models;
+using BraidsAccounting.Repos;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BraidsAccounting.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item>? Items { get; set; }
+        public Item? Item { get; set; } = new();
+
+
+        public MainViewModel()
+        {
+        }
+
+        //private ICommand? _CreateItemCommand;
+        //public ICommand? CreateItemCommand => _CreateItemCommand ??= new DelegateCommand<Item>(async (item) => Item = await ItemRepo.Create(item));
+
+        //private ICommand? _getItemsCommand;
+        //public ICommand? GetItemsCommand => _getItemsCommand ??= new DelegateCommand(async () => Items = new(await ItemRepo.GetAll()));
+
+        //private ICommand? _getItemCommand;
+        //public ICommand? GetItemCommand => _getItemCommand ??= new DelegateCommand<int?>(async (id) => Item = await ItemRepo.Get(id));
+
+        //private ICommand? _editItemCommand;
+        //public ICommand? EditItemCommand => _editItemCommand ??= new DelegateCommand<Item>(async (item) => 
+        //{
+        //    Item = await ItemRepo.Edit(item);
+        //    oncol
+        //});
+
+        //private ICommand? _deleteItemCommand;
+        //public ICommand? DeleteItemCommand => _deleteItemCommand ??= new DelegateCommand<int?>(async (id) => Item = await ItemRepo.Delete(id));
+    }
+
+}
+
