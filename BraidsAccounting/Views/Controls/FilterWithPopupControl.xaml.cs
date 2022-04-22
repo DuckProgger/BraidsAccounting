ae5d2f636dc0b4e595de3f42c566b133a1b0016f
Виталий Никитин <nikitin-vitalii@rambler.ru>
2022-04-22 14:19:24 +1000

    Добавлено расширение для удобной фильтрации DataGrid.

---
+++ BraidsAccounting/Views/Controls/FilterWithPopupControl.xaml.cs
@@ -0,0 +1,111 @@
+﻿namespace DataGridExtensionsSample.Controls
+{
+    using DataGridExtensions;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for FilterWithPopupControl.xaml
+    /// </summary>
+    public partial class FilterWithPopupControl
+    {
+        public FilterWithPopupControl()
+        {
+            InitializeComponent();
+        }
+
+        public string Caption
+        {
+            get => (string)GetValue(CaptionProperty);
+            set => SetValue(CaptionProperty, value);
+        }
+        /// <summary>
+        /// Identifies the Minimum dependency property
+        /// </summary>
+        public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register("Caption", typeof(string), typeof(FilterWithPopupControl)
+                , new FrameworkPropertyMetadata("Диапазон:", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public double Minimum
+        {
+            get => (double)GetValue(MinimumProperty);
+            set => SetValue(MinimumProperty, value);
+        }
+        /// <summary>
+        /// Identifies the Minimum dependency property
+        /// </summary>
+        public static readonly DependencyProperty MinimumProperty = DependencyProperty.Register("Minimum", typeof(double), typeof(FilterWithPopupControl)
+                , new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, (sender, _) => ((FilterWithPopupControl)sender).Range_Changed()));
+
+        public double Maximum
+        {
+            get => (double)GetValue(MaximumProperty);
+            set => SetValue(MaximumProperty, value);
+        }
+        /// <summary>
+        /// Identifies the Maximum dependency property
+        /// </summary>
+        public static readonly DependencyProperty MaximumProperty = DependencyProperty.Register("Maximum", typeof(double), typeof(FilterWithPopupControl)
+                , new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, (sender, _) => ((FilterWithPopupControl)sender).Range_Changed()));
+
+
+        public bool IsPopupVisible
+        {
+            get => (bool)GetValue(IsPopupVisibleProperty);
+            set => SetValue(IsPopupVisibleProperty, value);
+        }
+        /// <summary>
+        /// Identifies the IsPopupVisible dependency property
+        /// </summary>
+        public static readonly DependencyProperty IsPopupVisibleProperty =
+            DependencyProperty.Register("IsPopupVisible", typeof(bool), typeof(FilterWithPopupControl), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        private void Range_Changed() => Filter = Maximum > Minimum ? new ContentFilter(Minimum, Maximum) : null;
+
+        public IContentFilter? Filter
+        {
+            get => (IContentFilter?)GetValue(FilterProperty);
+            set => SetValue(FilterProperty, value);
+        }
+        /// <summary>
+        /// Identifies the Filter dependency property
+        /// </summary>
+        public static readonly DependencyProperty FilterProperty =
+            DependencyProperty.Register("Filter", typeof(IContentFilter), typeof(FilterWithPopupControl), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, (sender, _) => ((FilterWithPopupControl)sender).Filter_Changed()));
+
+
+        private void Filter_Changed()
+        {
+            if (Filter is not ContentFilter filter)
+                return;
+
+            Minimum = filter.Min;
+            Maximum = filter.Max;
+        }
+
+    }
+
+    public class ContentFilter : IContentFilter
+    {
+        public ContentFilter(double min, double max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public double Min { get; }
+
+        public double Max { get; }
+
+        public bool IsMatch(object? value)
+        {
+            if (value == null)
+                return false;
+
+            if (!double.TryParse(value.ToString(), out double number))
+            {
+                return false;
+            }
+
+            return (number >= Min) && (number <= Max);
+        }
+    }
+}
